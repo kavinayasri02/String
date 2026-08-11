a979d0f00d0338cd13dd31030f540e2104f0e34c
Kavinayasri J <kavinayasri02@gmail.com>
2026-08-11 23:32:15 +0530

    Add files via upload

---
+++ convertUpperCase.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class convertUpperCase {
+    public static void main(String[]args){
+        Scanner kavi = new Scanner(System.in);
+        String k = kavi.nextLine();
+        String UC = k.toUpperCase();
+        System.out.println("UpperCase word : " + UC);
+    }
+}
