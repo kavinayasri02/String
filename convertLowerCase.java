630a1b15608e6f8c32e131db1269d684e1cb31d2
Kavinayasri J <kavinayasri02@gmail.com>
2026-08-11 23:32:01 +0530

    Add files via upload

---
+++ convertLowerCase.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class convertLowerCase {
+    public static void main(String[]args){
+        Scanner kavi = new Scanner(System.in);
+        String s = kavi.nextLine();
+        String LC = s.toLowerCase();
+        System.out.print("LowerCase word : " + LC);
+    }
+}
