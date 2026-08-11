9f71cf35867aedf946e579daff3dbc857c826355
Kavinayasri J <kavinayasri02@gmail.com>
2026-08-11 23:34:53 +0530

    Add files via upload

---
+++ printeachChar.java
@@ -0,0 +1,8 @@
+public class printeachChar {
+    public static void main(String[]args){
+        String str = "Hello World";
+        for(int i =0;i<str.length();i++){
+            System.out.print(str.charAt(i)+ " ");
+        }
+    }
+}
