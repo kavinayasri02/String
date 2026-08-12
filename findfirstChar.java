5f3a1e423654e8567e315f900545c89c6c62c955
Kavinayasri J <kavinayasri02@gmail.com>
2026-08-12 23:07:24 +0530

    Add files via upload

---
+++ findfirstChar.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class findfirstChar {
+    public static void main(String[] args) {
+        Scanner kavi = new Scanner(System.in);
+        String k = kavi.nextLine();
+        char first = k.charAt(0);
+        System.out.print("First Character : " +  first);
+        }
+    }
