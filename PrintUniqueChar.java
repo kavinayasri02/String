4418849339f6846ee1aa68e1ae2180b4b756a81e
Kavinayasri J <kavinayasri02@gmail.com>
2026-08-18 23:55:21 +0530

    Add files via upload

---
+++ PrintUniqueChar.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class PrintUniqueChar {
+    public static void main(String[]args){
+        Scanner kavi = new Scanner(System.in);
+        String k = kavi.nextLine();
+        char[]ch = k.toCharArray();
+        System.out.println("Unique Characters:");
+        for(int i =0;i<ch.length;i++){
+            boolean isUnique = true;
+            for(int j =0;j<ch.length;j++){
+                if(i!= j&& ch[i] == ch[j]){
+                    isUnique = false;
+                    break;
+                }
+            }
+            if(isUnique){
+                System.out.print(ch[i] + " ");
+            }
+        }
+    }
+}
